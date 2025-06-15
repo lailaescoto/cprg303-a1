93a58d4f0c49995344a78beb54e21b03264e23d3
Laila Escoto <lailaangelicabayang.escoto@edu.sait.ca>
2025-06-14 23:17:22 -0600

    started profile page + has banner, profile picture, name, and @username for now

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,24 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
-const profile = () => {
+const ProfileScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Profile</Text>
-    </View>
+    <View>
+    {/* Banner / Header Image */}
+      <Image
+        source={{ uri: 'https://i.pravatar.cc/800?img=12' }}
+        style={styles.banner}
+      />
+
+      {/* Profile Picture */}
+      <View style={styles.profilePicContainer}>
+        <Image
+          source={{ uri: 'https://i.pravatar.cc/150?img=4' }}
+          style={styles.profilePic}
+        />
+      </View>
+      
+      {/* Name and Handle */}
+      <View style={styles.nameHandle}>
+        <Text style={styles.name}>Wikipedia</Text>
+        <Text style={styles.handle}>@Wikipedia</Text>
+      </View>
+      
+      </View>
+      
   )
 }
 
-export default profile
+export default ProfileScreen
 
 const styles = StyleSheet.create({
     container: {
     flex: 1,
     backgroundColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
+  banner: {
+    width: '100%',
+    height: 140,
+  },
+  profilePicContainer: {
+    position: 'absolute',
+    top: 90,
+    left: 16,
+    borderRadius: 50,
+    borderWidth: 4,
+    borderColor: '#000',
+  },
+  profilePic: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  nameHandle: {
+    marginTop: 60,
+    marginLeft: 16,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
     color: '#fff',
-    fontSize: 20,
+  },
+  handle: {
+    fontSize: 16,
+    color: '#aaa',
+    marginTop: 4,
   }
 })
